ef45f09720c140600187dfc44dd785791aaebbd1
Ritesh Sinha <riteshsinha@Riteshs-MacBook-Air.local>
2021-07-05 19:32:04 +0530

    App icon attached

---
+++ TeamPlayer/Controllers/CompareVC.swift
@@ -0,0 +1,24 @@
+//
+//  CompareVC.swift
+//  TeamPlayer
+//
+//  Created by Ritesh Sinha on 05/07/21.
+//
+
+import UIKit
+
+class CompareVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func menuAction(_ sender: Any) {
+        openSideMenu()
+    }
+    @IBAction func compareBtnAction(_ sender: Any) {
+    }
+    
+}
